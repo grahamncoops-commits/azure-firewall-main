5a7e68f4493bac01819e3932cc4c1c7be7b74fcf
unknown <grahamncoops@gmail.com>
2026-04-10 21:17:03 +0100

    Fix backend.tf storage account name

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "gcoopstfstateaccount"   # must be globally unique
+    container_name       = "tfstate"
+    key                  = "firewall.tfstate"
+  }
+}
