c95abc84a820fc01c18636a704fc6861d23f2b03
unknown <grahamncoops@gmail.com>
2026-04-15 19:39:56 +0100

    Fix Terraform formatting

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "gcoopstfstateaccount"   # must be globally unique
+    storage_account_name = "gcoopstfstateaccount" # must be globally unique
     container_name       = "tfstate"
     key                  = "firewall.tfstate"
   }
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "firewall_subnet" {
-  name                 = "AzureFirewallSubnet"   # must be this exact name
+  name                 = "AzureFirewallSubnet" # must be this exact name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/26"]
